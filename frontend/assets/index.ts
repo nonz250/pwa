809a08ff398cb04370940a170e4d9417aadc8671
nonz250 <hosaka.non.work@gmail.com>
2023-02-08 19:46:38 +0900

    update page.

--- frontend/assets/index.ts
+++ frontend/assets/index.ts
@@ -1,1 +1,9 @@
-console.log("test")
+console.log(
+  "%c This is pwa sample page for nonz250 learn.",
+  "padding: 0.2rem; border-radius: 5px; color: white; background-color: #3d50b7;",
+)
+
+console.log(
+  "You can refer to it if you like.\n%chttps://github.com/nonz250/pwa",
+  "padding: 0.2rem 0; border-radius: 5px; font-weight: 700; font-size: 0.8rem;",
+)
