f70d9fb0842e94eef99b7cbe45f6f0f31e2adec4
nonz250 <hosaka.non.work@gmail.com>
2023-02-13 22:45:06 +0900

    add service worker.

--- frontend/assets/index.ts
+++ frontend/assets/index.ts
@@ -7,3 +7,22 @@ console.log(
   "You can refer to it if you like.\n%chttps://github.com/nonz250/pwa",
   "padding: 0.2rem 0; border-radius: 5px; font-weight: 700; font-size: 0.8rem;",
 )
+
+window.onload = async () => {
+  if (!('serviceWorker' in navigator)) {
+    console.error('Service worker is disabled.')
+  }
+
+  try {
+    const registration = await navigator
+      .serviceWorker
+      .register('/dist/sw.js')
+
+    registration.onupdatefound = async () => {
+      console.log('Update service worker.')
+      await registration.update()
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
