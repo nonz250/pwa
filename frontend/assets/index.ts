9e2f140779f6016e9dac719b8ee98a7bae9684c2
nonz250 <hosaka.non.work@gmail.com>
2023-02-18 02:31:17 +0900

    eslint.

--- frontend/assets/index.ts
+++ frontend/assets/index.ts
@@ -1,11 +1,11 @@
 console.log(
-  "%cThis is pwa sample page for nonz250 learn.",
-  "padding: 0.2rem; border-radius: 5px; color: white; background-color: #3d50b7;",
+  '%cThis is pwa sample page for nonz250 learn.',
+  'padding: 0.2rem; border-radius: 5px; color: white; background-color: #3d50b7;'
 )
 
 console.log(
-  "You can refer to it if you like.\n%chttps://github.com/nonz250/pwa",
-  "padding: 0.2rem 0; border-radius: 5px; font-weight: 700; font-size: 0.8rem;",
+  'You can refer to it if you like.\n%chttps://github.com/nonz250/pwa',
+  'padding: 0.2rem 0; border-radius: 5px; font-weight: 700; font-size: 0.8rem;'
 )
 
 window.onload = async () => {
@@ -22,8 +22,7 @@ window.onload = async () => {
     console.error(error)
   }
 
-  document.getElementById('notification')?.addEventListener('click', async () => {
-    const permission = await Notification.requestPermission()
-    console.log(permission)
+  document.getElementById('notification')?.addEventListener('click', () => {
+    void Notification.requestPermission()
   })
 }
--- frontend/assets/sw.ts
+++ frontend/assets/sw.ts
@@ -10,49 +10,66 @@ const CURRENT_CACHES = {
 }
 const MAIN_CACHES = [
   '/',
-  '/dist/index.js',
+  '/dist/index.js'
 ]
 
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
   console.log('install', event)
-  caches.open(CACHE_NAME).then(cache => {
-    cache.addAll(MAIN_CACHES)
-  })
-})
-
-self.addEventListener('activate', async (event) => {
-  console.log('activate', event)
-  if (Notification.permission === 'granted') {
-    await self.registration.showNotification('New PWA Sample activate.', notificationBody)
+  const addAllCache = async (): Promise<void> => {
+    const cache = await caches.open(CACHE_NAME)
+    await cache.addAll(MAIN_CACHES)
   }
-  // @see https://developer.mozilla.org/ja/docs/Web/API/Cache
-  const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
-  caches.keys().then((cacheNames) =>
-    Promise.all(
-      cacheNames.map((cacheName) => {
-        if (!expectedCacheNamesSet.has(cacheName)) {
-          console.log(`Update caches ${cacheName} to ${CACHE_NAME}`);
-          return caches.delete(cacheName);
-        }
-      })
-    )
-  );
+  event.waitUntil(addAllCache())
 })
 
-self.addEventListener('sync', async (event) => {
+self.addEventListener('activate', (event) => {
+  console.log('activate', event)
+
+  const notificationActivate = async (): Promise<void> => {
+    if (Notification.permission === 'granted') {
+      await self.registration.showNotification('New PWA Sample activate.', notificationBody)
+    }
+  }
+
+  const updateCaches = async (): Promise<void> => {
+    // @see https://developer.mozilla.org/ja/docs/Web/API/Cache
+    const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES))
+    void caches.keys().then(async (cacheNames) =>
+      await Promise.all(
+        cacheNames.map(async (cacheName): Promise<Promise<boolean> | Promise<void>> => {
+          if (!expectedCacheNamesSet.has(cacheName)) {
+            console.log(`Update caches ${cacheName} to ${CACHE_NAME}`)
+            return await caches.delete(cacheName)
+          }
+        })
+      )
+    )
+  }
+
+  const activate = async (): Promise<void> => {
+    await updateCaches()
+    await notificationActivate()
+  }
+
+  void activate()
+})
+
+self.addEventListener('sync', (event) => {
   console.log('sync', event)
 })
 
-self.addEventListener('fetch', async (event) => {
+self.addEventListener('fetch', (event) => {
   console.log('fetch', event)
   console.log(navigator.onLine)
 })
 
-self.addEventListener('offline', async (event) => {
+self.addEventListener('offline', (event) => {
   console.log('offline', event)
 })
 
-self.addEventListener('push', async (event) => {
+self.addEventListener('push', (event) => {
   console.log('push', event)
-  await self.registration.showNotification(event.data!.text(), notificationBody)
+  if (event.data !== null) {
+    void self.registration.showNotification(event.data.text(), notificationBody)
+  }
 })
